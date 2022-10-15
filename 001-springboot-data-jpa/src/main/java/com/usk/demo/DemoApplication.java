3af12f25fe2e5725146081497684158db16576c3
gangadhar.setti7@gmail.com <gangadhar.setti7@gmail.com>
2022-10-15 15:15:23 +0530

    srinu changes

---
+++ 001-springboot-data-jpa/src/main/java/com/usk/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.usk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//change
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+}
